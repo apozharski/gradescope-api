d198dbb4b3a66ffc41a00a4bdfd33d56a751fee3
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2020-01-16 13:44:25 -0500

    Added question adding functionality

--- pyscope/assignment.py
+++ pyscope/assignment.py
@@ -17,7 +17,38 @@ class GSAssignment():
         self.regrades_on = regrades_on
         self.course = course
         self.questions = []
-        
+
+    def add_question(self, title, weight, crop = None, content = [], parent_id = None):
+        new_q_data = [q.to_patch() for q in self.questions]
+        new_crop = crop if crop else [{'x1': 10, 'x2': 91, 'y1': 73, 'y2': 93, 'page_number': 1}]
+        new_q = {'title': title, 'weight': weight, 'crop_rect_list': new_crop}
+        if parent_id:
+            # TODO: This should throw a custom exception if a parent is not found
+            parent = [parent for parent in new_q_data if parent['id'] == parent_id][0]
+            if parent['children']:
+                parent['children'].append(new_q)
+            else:
+                parent['children'] = [new_q]
+        else:
+            new_q_data.append(new_q)
+
+        new_patch = {'assignment': {'identification_regions': {'name': None, 'sid': None}},
+                     'question_data': new_q_data}
+
+        outline_resp = self.course.session.get('https://www.gradescope.com/courses/' + self.course.cid +
+                                               '/assignments/' + self.aid + '/outline/edit')
+        parsed_outline_resp = BeautifulSoup(outline_resp.text, 'html.parser')
+        authenticity_token = parsed_outline_resp.find('meta', attrs = {'name': 'csrf-token'} ).get('content')
+
+        patch_resp = self.course.session.patch('https://www.gradescope.com/courses/' + self.course.cid +
+                                               '/assignments/' + self.aid + '/outline/',
+                                               headers = {'x-csrf-token': authenticity_token,
+                                                          'Content-Type': 'application/json'},
+                                               data = json.dumps(new_patch,separators=(',',':')))
+        print(patch_resp.status_code)
+        print(patch_resp.request.headers)
+        print(patch_resp.request.body)
+    
     # TODO
     def add_instructor_submission(self, fname):
         '''
@@ -33,8 +64,7 @@ class GSAssignment():
     def unpublish_grades(self):
         pass
 
-    def _lazy_load_questions(self):
-        
+    def _lazy_load_questions(self):        
         outline_resp = self.course.session.get('https://www.gradescope.com/courses/' + self.course.cid +
                                                '/assignments/' + self.aid + '/outline/edit')
         parsed_outline_resp = BeautifulSoup(outline_resp.text, 'html.parser')
@@ -50,7 +80,7 @@ class GSAssignment():
             parent_id = question['parent_id']
             weight = question['weight']
             content = question['content']
-
+            crop = question['crop_rect_list']
             children = []
             qchildren = question.get('children', [])
             
@@ -60,5 +90,8 @@ class GSAssignment():
                 c_parent_id = subquestion['parent_id']
                 c_weight = subquestion['weight']
                 c_content = subquestion['content']
-                children.append(GSQuestion(c_qid, c_title, c_weight, [], c_parent_id, c_content))
-            self.questions.append(GSQuestion(qid, title, weight, children, parent_id, content))
+                c_crop = subquestion['crop_rect_list']
+                children.append(GSQuestion(c_qid, c_title, c_weight, [], c_parent_id, c_content, c_crop))
+            self.questions.append(GSQuestion(qid, title, weight, children, parent_id, content, crop))
+            
+        
--- pyscope/question.py
+++ pyscope/question.py
@@ -2,13 +2,20 @@ from bs4 import BeautifulSoup
 
 class GSQuestion():
 
-    def __init__(self, qid, title, weight, children, parent_id, content):
+    def __init__(self, qid, title, weight, children, parent_id, content, crop):
         '''Create a assignment object'''
         self.title = title
         self.qid = qid
-        self.children = []
+        self.children = children
         self.weight = weight
         self.parent_id = parent_id
         self.content = content
+        self.crop = crop
         
-    
+    def to_patch(self):
+        children = [child.to_patch() for child in self.children]
+        output = {'id': self.qid, 'title': self.title, 'weight': self.weight, 'crop_rect_list': self.crop}
+        print('length of children:', len(self.children))
+        if len(children) != 0:
+            output['children'] = children
+        return output
